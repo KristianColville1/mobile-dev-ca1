20474a745e521f35f2148c04113ecc49b99927e5
KristianColville1 <kristiancolville1@gmail.com>
2024-06-04 18:43:17 +0100

    Added welcome activity and base set up

---
+++ app/src/main/java/com/example/ca1_mobile_dev/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ca1_mobile_dev.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
